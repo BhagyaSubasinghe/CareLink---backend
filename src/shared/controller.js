1954a045b6ccb77c98014e9db8c460245389ed00
Bhagya Subasinghe <22cis0327@ms.sab.ac.lk>
2026-06-03 18:57:23 +0530

    connect to mongodb

---
+++ src/shared/controller.js
@@ -0,0 +1,14 @@
+const user = require('../features/user/userModel');
+const doctor = require('../features/doctor/Doctor');
+const patient = require('../features/patient/Patient');
+const appointment = require('../features/appointment/Appointment');
+
+module.exports = {
+  user,
+  doctor,   
+
+    patient,
+
+    appointment
+};
+
